25bd596e6198814e52ff92bbd1adda0bbb2fbb6d
moinur-rahman <rahmanmoin27@gmail.com>
2023-10-16 11:27:52 +0600

    about us animation added

--- src/modules/AboutPage/components/AboutTechshore/AboutB2Work.tsx
+++ src/modules/AboutPage/components/AboutTechshore/AboutB2Work.tsx
@@ -11,13 +11,17 @@ interface IProps {
 
 const AboutB2Work = ({ title, description }: IProps) => {
   const aboutRef = useRef(null);
-
+  const founderRef = useRef(null);
+  const cultureRef = useRef(null);
+  const ceoRef = useRef(null);
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
           observer.unobserve(entry.target);
-          gsap.killTweensOf(".slide-in-left,.slide-in-right, .zoom-in-element");
+          gsap.killTweensOf(
+            ".slide-in-left,.slide-in-right, .zoom-in-element-about"
+          );
 
           const tl = gsap.timeline();
 
@@ -40,7 +44,7 @@ const AboutB2Work = ({ title, description }: IProps) => {
           );
 
           tl.from(
-            ".zoom-in-element",
+            ".zoom-in-element-about",
             {
               scale: 0.5,
               opacity: 0,
@@ -67,28 +71,148 @@ const AboutB2Work = ({ title, description }: IProps) => {
       }
     };
   }, []);
+
+  useEffect(
+    () => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            gsap.killTweensOf(".zoom-in-element-founder");
+
+            const tl = gsap.timeline();
+
+            tl.from(".zoom-in-element-founder", {
+              scale: 0.5,
+              opacity: 0,
+              duration: 1,
+              ease: "power2.out",
+              stagger: 0.5,
+            });
+          }
+        },
+        {
+          threshold: 0.1,
+        }
+      );
+
+      if (founderRef.current) {
+        observer.observe(founderRef.current);
+      }
+
+      return () => {
+        if (founderRef.current) {
+          observer.unobserve(founderRef.current);
+        }
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  useEffect(
+    () => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            gsap.killTweensOf(".zoom-in-element-culture");
+
+            const tl = gsap.timeline();
+
+            tl.from(".zoom-in-element-culture", {
+              scale: 0.5,
+              opacity: 0,
+              duration: 1,
+              ease: "power2.out",
+              stagger: 0.5,
+            });
+          }
+        },
+        {
+          threshold: 0.1,
+        }
+      );
+
+      if (cultureRef.current) {
+        observer.observe(cultureRef.current);
+      }
+
+      return () => {
+        if (cultureRef.current) {
+          observer.unobserve(cultureRef.current);
+        }
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  useEffect(
+    () => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            gsap.killTweensOf(".zoom-in-element-ceo");
+
+            const tl = gsap.timeline();
+
+            tl.from(".zoom-in-element-ceo", {
+              scale: 0.5,
+              opacity: 0,
+              duration: 1,
+              ease: "power2.out",
+              stagger: 0.25,
+            });
+          }
+        },
+        {
+          threshold: 0.1,
+        }
+      );
+
+      if (ceoRef.current) {
+        observer.observe(ceoRef.current);
+      }
+
+      return () => {
+        if (ceoRef.current) {
+          observer.unobserve(ceoRef.current);
+        }
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   return (
-    <div
-      ref={aboutRef}
-      className="flex flex-col justify-center items-center bg-white px-60 "
-    >
-      <section className="flex justify-center items-center w-full py-20">
-        <h1 className="text-heading2-bold mr-20 slide-in-left">{title}</h1>
-        <Image
-          priority
-          src="/images/about-us1.jpg"
-          alt="incubator"
-          width={600}
-          height={600}
-          className="rounded-3xl slide-in-right"
-        />
+    <div className="flex flex-col justify-center items-center bg-white px-60 ">
+      <section
+        ref={aboutRef}
+        className="flex flex-col justify-center items-center w-full pt-20 pb-10"
+      >
+        <div className="flex justify-center items-center ">
+          <h1 className="text-heading2-bold mr-20 slide-in-left">{title}</h1>
+          <Image
+            priority
+            src="/images/about-us1.jpg"
+            alt="incubator"
+            width={600}
+            height={600}
+            className="rounded-3xl slide-in-right"
+          />
+        </div>
+        <p className="px-64 pt-14 text-[1.1rem] zoom-in-element-about">
+          {description}
+        </p>
       </section>
       <div className="px-64 text-[1.1rem]">
-        <section>{description}</section>
-
-        <section>
-          <h1 className="text-heading1-semibold mt-10 mb-5">Founder Story</h1>
-          <p>
+        <section ref={founderRef}>
+          <h1 className="text-heading1-semibold mb-5 zoom-in-element-founder">
+            Founder Story
+          </h1>
+          <p className="zoom-in-element-founder">
             More than two decades ago, Ben Chestnut and Dan Kurzius started a
             web design agency called the Rocket Science Group. Their focus was
             on big, corporate clients, but on the side, they created a
@@ -108,9 +232,11 @@ const AboutB2Work = ({ title, description }: IProps) => {
             partner we wish we had way back in the beginning.
           </p>
         </section>
-        <section>
-          <h1 className="text-heading1-semibold mt-10 mb-5">Culture</h1>
-          <p>
+        <section ref={cultureRef}>
+          <h1 className="text-heading1-semibold mt-10 mb-5 zoom-in-element-culture">
+            Culture
+          </h1>
+          <p className="zoom-in-element-culture">
             Intuit Mailchimp strives to create a culture that empowers a humble,
             creative, and independent workforce. We are passionate about our
             small business customers and believe that collaboration and
@@ -129,8 +255,13 @@ const AboutB2Work = ({ title, description }: IProps) => {
           </p>
         </section>
       </div>
-      <section className="w-4/5 h-[50rem]  flex flex-col items-center">
-        <h1 className="text-heading2-bold mt-10 mb-5 ">About Our Ceo</h1>
+      <section
+        ref={ceoRef}
+        className="w-4/5 h-[50rem]  flex flex-col items-center"
+      >
+        <h1 className="text-heading2-bold mt-10 mb-5 zoom-in-element-ceo">
+          About Our Ceo
+        </h1>
         <div className="flex justify-around w-full py-10">
           <div className="flex flex-col items-center w-2/5">
             <Image
@@ -139,38 +270,38 @@ const AboutB2Work = ({ title, description }: IProps) => {
               alt="incubator"
               width={300}
               height={600}
-              className="rounded-3xl"
+              className="rounded-3xl zoom-in-element-ceo"
             />
-            <h2 className="text-heading4-bold text-gray-700 underline">
+            <h2 className="text-heading4-bold text-gray-700 underline zoom-in-element-ceo">
               ABC DEF
             </h2>
-            <i>CEO, Co-Founder</i>
+            <i className="zoom-in-element-ceo">CEO, Co-Founder</i>
             <div className=" text-gray-700 w-full flex justify-center my-7 ">
               <div className="flex w-1/3 justify-between">
                 <Link
                   href="https://web.facebook.com/novaworkio"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaFacebook size={24} />
                 </Link>
                 <Link
                   href="https://twitter.com/Novawork1o"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaTwitter size={24} />
                 </Link>
                 <Link
                   href="https://www.linkedin.com/company/novawork-io/"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaLinkedin size={24} />
                 </Link>
               </div>
             </div>
-            <p className="text-[1.1rem] text-justify">
+            <p className="text-[1.1rem] text-justify zoom-in-element-ceo">
               Casey lets Rand think he runs most of the show at SparkToro. He
               was formerly at Moz, Wistia, HubSpot working on Inbound.org, and
               most recently at Ookla. When not working, he enjoys spending time
@@ -185,38 +316,38 @@ const AboutB2Work = ({ title, description }: IProps) => {
               alt="incubator"
               width={300}
               height={600}
-              className="rounded-3xl"
+              className="rounded-3xl zoom-in-element-ceo"
             />
-            <h2 className="text-heading4-bold text-gray-700 underline">
+            <h2 className="text-heading4-bold text-gray-700 underline zoom-in-element-ceo">
               ABC DEF
             </h2>
-            <i>CEO, Co-Founder</i>
+            <i className="zoom-in-element-ceo">CEO, Co-Founder</i>
             <div className=" text-gray-700 w-full flex justify-center my-7 ">
               <div className="flex w-1/3 justify-between">
                 <Link
                   href="https://web.facebook.com/novaworkio"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaFacebook size={24} />
                 </Link>
                 <Link
                   href="https://twitter.com/Novawork1o"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaTwitter size={24} />
                 </Link>
                 <Link
                   href="https://www.linkedin.com/company/novawork-io/"
-                  className="mr-4 hover:text-gray-500"
+                  className="mr-4 hover:text-gray-500 zoom-in-element-ceo"
                   target="_blank"
                 >
                   <FaLinkedin size={24} />
                 </Link>
               </div>
             </div>
-            <p className="text-[1.1rem] text-justify">
+            <p className="text-[1.1rem] text-justify zoom-in-element-ceo">
               Casey lets Rand think he runs most of the show at SparkToro. He
               was formerly at Moz, Wistia, HubSpot working on Inbound.org, and
               most recently at Ookla. When not working, he enjoys spending time
