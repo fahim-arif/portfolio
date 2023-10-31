e8eddc793be0d3356c8ebcde715c809573a245ed
moinur-rahman <rahmanmoin27@gmail.com>
2023-10-31 09:12:33 +0600

    head added in thank you

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Navbar from "../src/common/elements/Navbar";
 import ThankYouSection from "../src/modules/ThankYouPage/ThankYouSection";
 
 const ThankYou = () => {
   return (
     <>
+      <Head>
+        <title>Thank You</title>
+      </Head>
       <Navbar />
       <ThankYouSection />
     </>
